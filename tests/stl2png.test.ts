0c2a4a615791462944a5d75c7aff4c1357b07326
Daniel Steingrube <daniel.steingrube@scalenc.com>
2022-05-02 12:45:15 +0200

    patch: make stl2png non-async

--- tests/stl2png.test.ts
+++ tests/stl2png.test.ts
@@ -12,7 +12,7 @@ describe(stl2png.name, function () {
     describe('default', () => {
       it(`for '${filename}'`, async () => {
         const stlData = await fs.promises.readFile(path.join(__dirname, 'data', filename));
-        const png = await stl2png(stlData);
+        const png = stl2png(stlData);
         await fs.promises.writeFile(path.join(__dirname, 'data', 'dump', `default_${filename}.png`), png);
         const expected = await fs.promises.readFile(path.join(__dirname, 'data', 'ref', `default_${filename}.png`));
         expect(png).deep.eq(expected);
@@ -26,7 +26,7 @@ describe(stl2png.name, function () {
         const stlData = await fs.promises.readFile(path.join(__dirname, 'data', filename));
         const material = makeBasicMaterial(0.7, 0x3097d1);
         material.overDraw = 0;
-        const png = await stl2png(stlData, {
+        const png = stl2png(stlData, {
           materials: [material],
           edgeMaterials: [],
         });
@@ -42,7 +42,7 @@ describe(stl2png.name, function () {
       it(`for '${filename}'`, async () => {
         const stlData = await fs.promises.readFile(path.join(__dirname, 'data', filename));
         const metal = await fs.promises.readFile(path.join(__dirname, 'data', 'textures', 'metal.jpg'));
-        const png = await stl2png(stlData, {
+        const png = stl2png(stlData, {
           materials: [makeLambertMaterial(0.2, makeTexture(metal, false)), makeBasicMaterial(0.7, 0x3097d1)],
           edgeMaterials: [makeEdgeMaterial(0.1, 0x287dad)],
         });
@@ -58,7 +58,7 @@ describe(stl2png.name, function () {
       it(`for '${filename}'`, async () => {
         const stlData = await fs.promises.readFile(path.join(__dirname, 'data', filename));
         const metal = await fs.promises.readFile(path.join(__dirname, 'data', 'textures', 'metal.jpg'));
-        const png = await stl2png(stlData, {
+        const png = stl2png(stlData, {
           materials: [makeLambertMaterial(0.2, makeTexture(metal, false)), makeNormalMaterial(0.4)],
           edgeMaterials: [makeEdgeMaterial(0.1, 0x287dad)],
         });
